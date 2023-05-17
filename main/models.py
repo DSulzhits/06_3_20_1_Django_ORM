894dce8203ba773e6f84a89ea9482bdb6256007e
DSulzhits <dsulzhits@gmail.com>
2023-05-17 15:21:36 +0300

    add class Student(models.Model):

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=150, verbose_name='имя')
+    last_name = models.CharField(max_length=150, verbose_name='фамилия')
+    avatar = models.ImageField(upload_to='students/', verbose_name='аватар')
+
+    def __str__(self):
+        return f'{self.first_name}, {self.last_name}'
+
+    class Meta:
+        verbose_name = 'студент'
+        verbose_name_plural = 'студенты'
+        ordering = ('last_name',)
