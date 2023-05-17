0a2362b059316ecefeb3696cdc334dfa50c58470
DSulzhits <dsulzhits@gmail.com>
2023-05-17 18:01:57 +0300

    Модели добавлены в admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import Student
+
+
+# admin.site.register(Student)
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+    search_fields = ('first_name', 'last_name')
