68ff6864d07cf4df531847901aa14e01270832bc
DSulzhits <dsulzhits@gmail.com>
2023-05-26 12:42:54 +0300

    Добавлены url-s

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from main.apps import MainConfig
-from main.views import index
+from main.views import index, contacts, students
 
 app_name = MainConfig.name
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('contacts/', contacts),
+    path('students/', students),
 ]
