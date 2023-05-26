8f1586993db5cdb5d6ddb1738249f4f083033ff8
DSulzhits <dsulzhits@gmail.com>
2023-05-26 12:40:16 +0300

    Добавлен шаблон base

--- main/views.py
+++ main/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 
+from main.models import Student
+
 
 def index(request):
+    context = {
+        'object_list': Student.objects.all()[:3],
+        'title': 'Список студентов'
+    }
+    return render(request, 'main/index.html', context)
+
+
+def students(request):
+    context = {
+        'object_list': Student.objects.all(),
+        'title': 'Список студентов'
+    }
+    return render(request, 'main/students.html', context)
+
+
+def contacts(request):
     if request.method == "POST":
         name = request.POST.get('name')
         email = request.POST.get('email')
         message = request.POST.get('message')
         print(f'User {name}, with email {email}, send message: {message}')
-    return render(request, 'main/index.html')
+    return render(request, 'main/contact.html')
