a9bd11dbbdfa14a2d7fa9c758f38dcab25abae82
DSulzhits <dsulzhits@gmail.com>
2023-06-05 17:56:38 +0300

    Добавлены контроллеры: StudentDetailView StudentCreateView StudentUpdateView StudentDeleteView
    а также добавлен фильтр активных записей

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.views import generic
 from main.models import Student
 
 
@@ -11,21 +12,60 @@ def index(request):
     return render(request, 'main/index.html', context)
 
 
-def students(request):
-    context = {
-        'object_list': Student.objects.all(),
+class StudentListView(generic.ListView):
+    model = Student
+    extra_context = {
         'title': 'Список студентов'
     }
-    return render(request, 'main/students.html', context)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(is_active=True)
+        return queryset
 
 
-def student(request, pk):
-    student_item = Student.objects.get(pk=pk)
-    context = {
-        'object': student_item,
-        'title': student_item.first_name + ' ' + student_item.last_name  # или так 'title': student_item
-    }
-    return render(request, 'main/student.html', context)
+# def students(request):
+#     context = {
+#         'object_list': Student.objects.all(),
+#         'title': 'Список студентов'
+#     }
+#     return render(request, 'main/students.html', context)
+
+
+class StudentDetailView(generic.DetailView):
+    model = Student
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        # context_data['title'] = context_data['object']
+        context_data['title'] = self.get_object()
+        return context_data
+
+
+# def student(request, pk):
+#     student_item = Student.objects.get(pk=pk)
+#     context = {
+#         'object': student_item,
+#         'title': student_item.first_name + ' ' + student_item.last_name  # или так 'title': student_item
+#     }
+#     return render(request, 'main/student.html', context)
+
+
+class StudentCreateView(generic.CreateView):
+    model = Student
+    fields = ('first_name', 'last_name',)
+    success_url = reverse_lazy('main:students_list')
+
+
+class StudentUpdateView(generic.UpdateView):
+    model = Student
+    fields = ('first_name', 'last_name',)
+    success_url = reverse_lazy('main:students_list')
+
+
+class StudentDeleteView(generic.DeleteView):
+    model = Student
+    success_url = reverse_lazy('main:students_list')
 
 
 def contacts(request):
