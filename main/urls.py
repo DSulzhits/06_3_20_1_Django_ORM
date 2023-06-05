8673f1533fe5613210618367bfc94de0fbe00e7c
DSulzhits <dsulzhits@gmail.com>
2023-06-05 17:56:58 +0300

    Добавлены urls для контроллеров: StudentDetailView StudentCreateView StudentUpdateView StudentDeleteView

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 
 from main.apps import MainConfig
-from main.views import index, contacts, students, student
+from main.views import index, contacts, StudentDetailView, StudentListView, StudentCreateView, StudentUpdateView, StudentDeleteView
 
 app_name = MainConfig.name
 
 urlpatterns = [
     path('', index, name='index'),
     path('contacts/', contacts, name='contacts'),
-    path('students/', students, name='students_list'),
-    path('student/<int:pk>/', student, name='student_item'),
+    path('students/', StudentListView.as_view(), name='students_list'),
+    path('student/<int:pk>/', StudentDetailView.as_view(), name='student_item'),
+    path('students/create/', StudentCreateView.as_view(), name='students_create'),
+    path('students/update/<int:pk>/', StudentUpdateView.as_view(), name='students_update'),
+    path('students/delete/<int:pk>/', StudentDeleteView.as_view(), name='students_delete'),
 ]
