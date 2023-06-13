bac7b374cec5c8e893f75a82a5d7b067807ca3cc
DSulzhits <dsulzhits@gmail.com>
2023-06-13 22:04:03 +0300

    Расширен функционал админки

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from main.models import Student
+from main.models import Student, Subject
 
 
 # admin.site.register(Student)
@@ -9,3 +9,9 @@ class StudentAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name',)
     search_fields = ('first_name', 'last_name',)
     list_filter = ('is_active',)
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'student')
+    list_filter = ('student',)
