f796978c00f25efbdf1479c7bd82ea2ee7b38bdf
DSulzhits <dsulzhits@gmail.com>
2023-06-13 21:58:58 +0300

    Внесены изменения в модель Student, добавлено поле email, добавлена модель Subject, проведены необходимые миграции

--- main/models.py
+++ main/models.py
@@ -7,8 +7,10 @@ NULLABLE = {'blank': True, 'null': True}
 class Student(models.Model):
     first_name = models.CharField(max_length=150, verbose_name='имя')  # обязательно
     last_name = models.CharField(max_length=150, verbose_name='фамилия')  # обязательно
-    avatar = models.ImageField(upload_to='students/', verbose_name='аватар', **NULLABLE)  # не обязательно т.к. есть **NULLABLE
-
+    avatar = models.ImageField(upload_to='students/', verbose_name='аватар',
+                               **NULLABLE)  # не обязательно т.к. есть **NULLABLE
+    # для email у моделей есть специяльное поле, здесь такой метод применен для эксперимента
+    email = models.CharField(max_length=150, verbose_name='@email', unique=True, **NULLABLE)
     comment = models.TextField(verbose_name='комментарий менеджера', **NULLABLE)
 
     is_active = models.BooleanField(default=True, verbose_name='активный')
@@ -25,3 +27,16 @@ class Student(models.Model):
         verbose_name = 'студент'
         verbose_name_plural = 'студенты'
         ordering = ('last_name',)
+
+
+class Subject(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, verbose_name='студент')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'предмет'
+        verbose_name_plural = 'предметы'
