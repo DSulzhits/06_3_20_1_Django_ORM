04fb5778acb3c63f3eeba609705569fb95f38f91
DSulzhits <dsulzhits@gmail.com>
2023-06-29 17:23:46 +0300

    Кеширование части контроллера добавлено в сервисную прослойку

--- main/services.py
+++ main/services.py
@@ -1,5 +1,7 @@
 from django.core.mail import send_mail
 from django.conf import settings
+from django.core.cache import cache
+from main.models import Subject
 
 
 def send_deactivate_email(user):
@@ -9,3 +11,16 @@ def send_deactivate_email(user):
         settings.EMAIL_HOST_USER,
         recipient_list=[user.email]
     )
+
+
+def get_cached_subjects_for_student(student_pk):
+    if settings.CACHE_ENABLED:
+        key = f'subject_list_{student_pk}'
+        subject_list = cache.get(key)
+        if subject_list is None:
+            subject_list = Subject.objects.filter(student__pk=student_pk)
+            cache.set(key, subject_list)
+    else:
+        subject_list = Subject.objects.filter(student__pk=student_pk)
+
+    return subject_list
